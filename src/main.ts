c150a05dad8c94349a7ae99cbda0f36bbb5782ea
shinto <shinto.philip007@gmail.com>
2023-02-19 20:20:53 +0100

    add validation for create-coffee dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /* The ValidationPipe provides a convenient way of enforcing validation rules
+     for all incoming client payloads.
+     You can specify these rules by using simple annotations in your DTO! */
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
