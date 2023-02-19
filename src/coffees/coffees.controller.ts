d2fe6550818b9d42ea276eb1641cafa0503ca255
shinto <shinto.philip007@gmail.com>
2023-02-19 16:51:26 +0100

    try Res to set status code and body

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,25 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
+  /*
+  @Res() param decorator
+  Using underlying platform Response objects (from Express.js or Fastify)
+
+  🚨 Remember to use this with caution (as our code can become platform-dependent)
+*/
+
   @Get()
-  findAll() {
-    return 'This action returns all coffees';
+  findAll(@Res() res: any) {
+    return res.status(HttpStatus.OK).send('This action returns all coffees');
   }
 
   @Get(':id')
